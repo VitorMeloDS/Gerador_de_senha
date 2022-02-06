b134d5592d99dfa18ec0de02d27f524b6fc5f8b1
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-02-06 18:32:56 -0300

    Atualização do código

--- Gerador de senha/gerador.py
+++ Gerador de senha/gerador.py
@@ -3,16 +3,16 @@ import random
 #Listas com as letras caracteres e numeros para gerar a senha
 letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-caracteres = ["!", "@", "#", "$", "&", "-", "+", "*", "/", "=", "<", ">"]
+caracteres = ["!", "@", "#", "$", "&"]
 
 numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 #Entrada com os numeros de digitos que a senha possue
 tamanho = input("""
-    \033[7:30:47m                 Gerador de senha                  \033[m
-    \033[1:30:47m===================================================\033[m
-    \033[1:30:47mTamnhos de senhas suportadas com 4, 6 ou 8 digitos!\033[m
-    \033[0:30:47m                                                   \033[m
+    \033[7:30:47m                    Gerador de senha                    \033[m
+    \033[1:30:47m========================================================\033[m
+    \033[1:30:47mTamanhos de senhas suportadas são com 4, 6 ou 8 digitos!\033[m
+    \033[0:30:47m                                                        \033[m
     \033[4:30:47mDigite o tamanho da senha que deseja:\033[m""").strip()
 
 #If para a senah de 4 digitos 
@@ -24,10 +24,10 @@ if tamanho == "4":
         senha.append(sorteio)
         contador+=1
     print("""
-    \033[7:30:47m     senha de 4 digitos!      \033[m
-    \033[1:30:47m==============================\033[m
-    \033[0:30:47m                              \033[m
-    \033[1:30:42mOlá usuário a sua senha é:{}{}{}{}\033[m
+    \033[7:30:47m      senha de 4 digitos!      \033[m
+    \033[1:30:47m===============================\033[m
+    \033[0:30:47m                               \033[m
+    \033[1:30:42mOlá usuário a sua senha é: {}{}{}{}\033[m
     """.format(senha[0], senha[1], senha[2], senha[3]))
 
 #Elif para a senha de 6 digitos 
@@ -50,10 +50,10 @@ elif tamanho == "6":
             senha.append(sorteio)
             contador+=1
     print("""
-    \033[7:30:47m       senha de 6 digitos!      \033[m
-    \033[1:30:47m================================\033[m
-    \033[0:30:47m                                \033[m
-    \033[1:30:42mOlá usuário a sua senha é:{}{}{}{}{}{}\033[m
+    \033[7:30:47m       senha de 6 digitos!       \033[m
+    \033[1:30:47m=================================\033[m
+    \033[0:30:47m                                 \033[m
+    \033[1:30:42mOlá usuário a sua senha é: {}{}{}{}{}{}\033[m
     """.format(senha[0], senha[1], senha[2], senha[3], senha[4], senha[5]))
 
 #Elif para a senha de 8 digitos
@@ -61,25 +61,25 @@ elif tamanho == "8":
     contador = 0 
     senha = []
     while contador < 8:
-        if contador < 3:
+        if contador <= 3:
             sorteio = random.choice(letras)
             senha.append(sorteio)
             contador+=1
 
-        if contador == 3 or contador == 4:
+        if contador == 4 or contador == 5:
             sorteio = random.choice(caracteres)
             senha.append(sorteio)
             contador+=1
 
-        if contador >= 5:
+        if contador > 5:
             sorteio = random.choice(numeros)
             senha.append(sorteio)
             contador+=1
     print("""
-    \033[7:30:47m        senha de 8 digitos!       \033[m
-    \033[1:30:47m==================================\033[m
-    \033[0:30:47m                                  \033[m
-    \033[1:30:42mOlá usuário a sua senha é:{}{}{}{}{}{}{}{}\033[m
+    \033[7:30:47m        senha de 8 digitos!        \033[m
+    \033[1:30:47m==================================+\033[m
+    \033[0:30:47m                                   \033[m
+    \033[1:30:42mOlá usuário a sua senha é: {}{}{}{}{}{}{}{}\033[m
     """.format(senha[0], senha[1], senha[2], senha[3], senha[4], senha[5], senha[6], senha[7]))
 
 #Else para caso o usuário digite algo em não esta no padrão dos digitos da senhas
